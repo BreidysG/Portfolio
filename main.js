0f349c13be75e4803d39688eb23e9dc4b3af0bd7
BreidysG <breidysgutierrez8@gmail.com>
2023-07-04 08:11:14 -0400

    Add joins

--- main.js
+++ main.js
@@ -6,6 +6,14 @@ console.log(navLinks);
 let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
+const typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "Ingeniero de Sistemas"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true
+});
+
 
 window.onscroll = () =>{
     sections.forEach(sec => {
@@ -32,3 +40,16 @@ menuIcon.onclick = () =>{
     menuIcon.classList.toggle("bx-x");
     navbar.classList.toggle("active");
 }
+
+ScrollReveal({ 
+    //reset: true,
+    distance: "80px",
+    duration: 2000,
+    delay:200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: "top" });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: "bottom" });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: "left" });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: "right" });
+
